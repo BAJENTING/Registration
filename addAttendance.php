30d60058507cc7c923ba3d38a8d0a8c24ade441a
John Michael Vincent Bajenting <bajentingjohn@gmail.com>
2025-11-19 20:06:04 +0800

    Notes for attendance

--- addAttendance.php
+++ addAttendance.php
@@ -11,6 +11,25 @@
 
     $idNum = $_POST['idNum'];
     
+    $check = "SELECT attended from registration where idNum = '$idNum'";
+    $result = $conn->query($check);
+
+    if ($result->num_rows == 0){
+        echo "<script>
+            alert('ID # is not YET REGISTERED');
+            window.location.href = 'viewAttendance.php';
+        </script>";
+        exit;
+    }
+    $row = $result->fetch_assoc();
+
+    if ($row['attended'] === 'Yes'){
+        echo "<script>
+            alert('Student Attendance is already Recorded.');
+            window.location.href = 'viewAttendance.php';
+        </script>";
+        exit;
+    }
     $sql = "UPDATE registration SET attended = 'Yes' WHERE idNum = '$idNum'";
     if ($conn->query($sql)){
         echo "<script>
